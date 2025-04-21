da78ff0c5436f6f1bf0fd453b26dd927e0603341
Ξhsan (3H) <43131138+Ehsan-Habib-3H@users.noreply.github.com>
2025-04-22 02:17:04 +0330

    chore: add comment

--- src/main/java/org/example/javafx/PricingController.java
+++ src/main/java/org/example/javafx/PricingController.java
@@ -1,15 +1,11 @@
 package org.example.javafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
 public class PricingController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick()
-    {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    //TODO: Show message box as user clicks on "shop" button.(*Bonus*: Design another scene and change to it)
 }
